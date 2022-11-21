998b6a4ba28f53918d115181ce187efb98192242
otabeksherman <n.v.sherman@gmail.com>
2022-11-21 15:05:19 +0200

    add document table, support document creation and content editing

---
+++ src/main/java/docSharing/controller/DocumentManagementController.java
@@ -0,0 +1,29 @@
+package docSharing.controller;
+
+import docSharing.service.AuthenticationService;
+import docSharing.service.DocumentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("api/v1/doc")
+public class DocumentManagementController {
+
+    @Autowired
+    private DocumentService documentService;
+    @Autowired
+    AuthenticationService authenticationService;
+
+    @PostMapping("/create")
+    public void createDocument(@RequestParam String title,
+                               @RequestParam String token) {
+        try {
+            Long id = authenticationService.isLoggedIn(token);
+            documentService.createDocument(id, title);
+        } catch (IllegalStateException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Not logged in");
+        }
+    }
+}
